8e45a08c94c353e8c70105bec98963d9d77b5d3e
PedroAnuncio <p.a.d.moraes2003@hotmail.com>
2025-09-09 18:02:25 -0300

    bloco e collider consertados.

--- Assets/Scripts/blocks/breaking.cs
+++ Assets/Scripts/blocks/breaking.cs
@@ -2,38 +2,22 @@ using UnityEngine;
 
 public class breaking : MonoBehaviour
 {
-    public Sprite[] blockStates;
-    private int hits = 0;
-    private SpriteRenderer sr;
+    Rigidbody2D _rb;
+    //Animator _animator;
+    private Collider2D _collider;
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        //_animator = GetComponent<Animator>();
+        _collider = GetComponentInChildren<Collider2D>();
+    }
     
-    public GameObject hitter;
-
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        sr = GetComponent<SpriteRenderer>();
-        sr.sprite = blockStates[hits];
-    }
-
-    public void TakeHit()
-    {
-        hits++;
-
-        if (hits < blockStates.Length)
-        {
-            sr.sprite = blockStates[hits];
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject == hitter)
-        {
-            print($"{hitter.name} bateu!");
-            TakeHit();
-        }
+        print("aaaaaaaaaaaa");
+        //_animator.enabled = true;
+        _collider.enabled = false;
+        //Destroy(collision.transform.parent.gameObject);
+        Destroy(gameObject);
     }
 }
